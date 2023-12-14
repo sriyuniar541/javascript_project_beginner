fc41d452388eb99c927c9ef5ce4b569783b07969
sri yuniar <sriyuniar541@gmail.com>
2023-12-15 08:43:16 +0900

    update css

--- weather_app/index.js
+++ weather_app/index.js
@@ -13,11 +13,18 @@ let weather = {
         const {temp_c}     =  data.current; 
         const {humidity}   =  data.current; 
         const {wind_kph}   =  data.current; 
+        const {icon}       =  data.current.condition; 
+        const {text}       =  data.current.condition; 
 
         document.querySelector(".city").innerHTML       = name ;                         //weather
         document.querySelector(".temp").innerHTML       = Math.round(temp_c) + '°C';     //temp
         document.querySelector(".humidity").innerHTML   = humidity + '%';                //humidity
-        document.querySelector(".wind").innerHTML       = wind_kph + ' km/h';            //wind        
+        document.querySelector(".wind").innerHTML       = wind_kph + ' km/h';            //wind    
+        document.querySelector(".weather-icon").src     = icon;                          //icon
+        document.querySelector(".text").innerHTML       = text ;   
+        
+        console.log(icon);
+        
     },
 
     search : function () {
