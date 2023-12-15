66a64ef9b03df6903e9bab652002ce804f78674f
sri yuniar <sriyuniar541@gmail.com>
2023-12-15 14:10:58 +0900

    clock app

---
+++ digital_clock/index.js
@@ -0,0 +1,17 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+
+//untuk mendapatkan update terbaru setiap satu detik
+setInterval(() => {
+
+    let currentTime = new Date();
+
+    hrs.innerHTML = (currentTime.getHours()<10?"0":"" + currentTime.getHours());
+    min.innerHTML = (currentTime.getMinutes()<10?"0":"" + currentTime.getMinutes());
+    sec.innerHTML = (currentTime.getSeconds()<10?"0":"" + currentTime.getSeconds());
+
+}, 1000)
+
+
